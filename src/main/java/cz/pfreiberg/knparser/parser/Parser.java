b2c036b393d15ec23a83459489c15c6e0220358f
Petr Freiberg <pfreiberg@pfreiberg.dna.intra>
2013-12-04 08:58:29 +0100

    Parser.

--- src/main/java/cz/pfreiberg/knparser/parser/Parser.java
+++ src/main/java/cz/pfreiberg/knparser/parser/Parser.java
@@ -1,9 +1,14 @@
 package cz.pfreiberg.knparser.parser;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import cz.pfreiberg.knparser.Configuration;
 import cz.pfreiberg.knparser.domain.Vfk;
@@ -15,10 +20,7 @@ public class Parser {
 	Configuration configuration;
 	File file;
 	Vfk vfk;
-	Scanner scanner;
-	String actualLine;
-
-	private final String EOF = "\r\n";
+	BufferedReader br;
 
 	public Parser(Configuration configuration) throws FileNotFoundException,
 			ParserException, IOException {
@@ -26,94 +28,134 @@ public class Parser {
 		file = new File(configuration.getPathToFile());
 		vfk = new Vfk();
 		vfk.setCodepage(VfkUtil.getEncoding(file));
-		scanner = getScanner(file, vfk.getCodepage());
-		scanner.useDelimiter(EOF);
+		br = new BufferedReader(new InputStreamReader(
+				new FileInputStream(file), getEncoding(vfk.getCodepage())));
 	}
 
-	public void parse() {
-		actualLine = "";
-		while (scanner.hasNext()) {
-			actualLine = scanner.next();
-			String actualNode = actualLine.split(";")[0];
-			actualLine = actualLine.replace(actualNode + ";", "");
-			actualLine += "; ";
+	public void parse() throws IOException, ParserException {
+		for (String[] values = processNextRow(); values != null; values = processNextRow()) {
+			System.out.println(Arrays.asList(values));
+		}
+	}
 
-			switch (actualNode) {
-			case "&DPAR":
-				vfk.getParcely().add(ParcelyParser.parse(actualLine));
-				break;
-			case "&DBUD":
-				vfk.getBudovy().add(BudovyParser.parse(actualLine));
-				break;
-			case "&DCABU":
-				vfk.getCastiBudov().add(CastiBudovParser.parse(actualLine));
-				break;
-			case "&DZPOCHN":
-				vfk.getZpOchranyNem().add(ZpOchranyNemParser.parse(actualLine));
-				break;
-			case "&DDRUPOZ":
-				vfk.getDPozemku().add(DPozemkuParser.parse(actualLine));
-				break;
-			case "&DZPVYPO":
-				vfk.getZpVyuzitiPoz().add(ZpVyuzitiPozParser.parse(actualLine));
-				break;
-			case "&DZDPAZE":
-				vfk.getZdrojeParcelZe().add(
-						ZdrojeParcelZeParser.parse(actualLine));
-				break;
-			case "&DZPURVY":
-				vfk.getZpUrceniVymery().add(
-						ZpUrceniVymeryParser.parse(actualLine));
-				break;
-			case "&DTYPBUD":
-				vfk.getTBudov().add(TBudovParser.parse(actualLine));
-				break;
-			case "&DMAPLIS":
-				vfk.getMapoveListy().add(MapoveListyParser.parse(actualLine));
-				break;
-			case "&DKATUZE":
-				vfk.getKatastrUzemi().add(KatastrUzemiParser.parse(actualLine));
-				break;
-			case "&DOBCE":
-				vfk.getObce().add(ObceParser.parse(actualLine));
-				break;
-			case "&DCASOBC":
-				vfk.getCastiObci().add(CastiObciParser.parse(actualLine));
-				break;
-			case "&DOKRESY":
-				vfk.getOkresy().add(OkresyParser.parse(actualLine));
-				break;
-			case "&DKRAJE":
-				vfk.getKraje().add(KrajeParser.parse(actualLine));
-				break;
-			case "&DNKRAJE":
-				vfk.getNoveKraje().add(NoveKrajeParser.parse(actualLine));
-				break;
-			case "&DRZO":
-				vfk.getRZpochr().add(RZpochrParser.parse(actualLine));
-				break;
-			case "&DZPVYBU":
-				vfk.getZpVyuzitiBud().add(ZpVyuzitiBudParser.parse(actualLine));
-				break;
-
+	public String[] processNextRow() throws IOException {
+		String[] row = null;
+		do {
+			String nextRow = getNextRow();
+			if (nextRow == null)
+				return row;
+			String[] processedRow = parseRow(nextRow);
+			if (processedRow.length > 0) {
+				if (row == null) {
+					row = processedRow;
+				} else {
+					String[] temp = new String[row.length + processedRow.length];
+					System.arraycopy(row, 0, temp, 0, row.length);
+					System.arraycopy(processedRow, 0, temp, row.length,
+							processedRow.length);
+					row = temp;
+				}
 			}
-		}
-		System.out.println("End of file.");
-
-		// TODO testovací výpis
-		for (int i = 0; i < 5; i++) {
-			System.out.println(vfk.getParcely().get(i));
-		}
-
+			return row;
+		} while (true);
 	}
 
-	private Scanner getScanner(File file, String codepage)
-			throws ParserException, FileNotFoundException {
-		return new Scanner(file, getEncodingForScanner(codepage));
+	public String getNextRow() throws IOException {
+		String nextRow = br.readLine();
+		return nextRow;
 	}
 
-	private String getEncodingForScanner(String codepage)
-			throws ParserException {
+	private final char escapeCharacter = '\\';
+	private final char quoteCharacter = '"';
+	private final char separator = ';';
+	private String buffer;
+
+	public String[] parseRow(String row) {
+
+		List<String> tokensOnRow = new ArrayList<String>();
+		StringBuilder sb = new StringBuilder(64);
+		boolean inQuotes = false;
+
+		if (buffer != null) {
+			sb.append(buffer);
+			buffer = null;
+			inQuotes = true;
+		}
+
+		for (int i = 0; i < row.length(); i++) {
+			char actualCharacter = getActualCharacter(row, i);
+
+			switch (actualCharacter) {
+			case escapeCharacter:
+				if (isNextCharacterEscapable(row, inQuotes, i)) {
+					sb.append(getNextCharacter(row, i));
+					i++;
+				}
+				sb.append(actualCharacter);
+				break;
+			case quoteCharacter:
+				sb.append(actualCharacter);
+				inQuotes = !inQuotes;
+				break;
+			case separator:
+				if (!inQuotes) {
+					tokensOnRow.add(sb.toString());
+					sb.setLength(0);
+				} else
+					sb.append(actualCharacter);
+				break;
+			default:
+				sb.append(actualCharacter);
+				break;
+			}
+
+		}
+
+		if (inQuotes) {
+			sb.append("\n");
+			buffer = sb.toString();
+		}
+
+		if (sb != null) {
+			tokensOnRow.add(sb.toString());
+		}
+
+		sb = null;
+		return tokensOnRow.toArray(new String[tokensOnRow.size()]);
+	}
+
+	public boolean isNextCharacterEscapable(String row, boolean inQuotes,
+			int position) {
+		if (hasNextCharacter(row, position)) {
+			char nextCharacter = getNextCharacter(row, position);
+			return (inQuotes && (nextCharacter == quoteCharacter) || (nextCharacter == escapeCharacter));
+		}
+		return false;
+	}
+
+	public boolean isNextCharacterSeparator(String row, int position) {
+		if (hasNextCharacter(row, position)) {
+			char nextCharacter = getNextCharacter(row, position);
+			return (nextCharacter == separator);
+		}
+		return false;
+	}
+
+	public char getActualCharacter(String row, int position) {
+		return row.charAt(position);
+	}
+
+	public boolean hasNextCharacter(String row, int position) {
+		position++;
+		return (row.length() > position);
+	}
+
+	public char getNextCharacter(String row, int position) {
+		position++;
+		return row.charAt(position);
+	}
+
+	private String getEncoding(String codepage) throws ParserException {
 		if (EncodingCzech.windows1250.equalsVfk(codepage)) {
 			return EncodingCzech.windows1250.getEncoding();
 		} else if (EncodingCzech.iso88592.equalsVfk(codepage)) {
